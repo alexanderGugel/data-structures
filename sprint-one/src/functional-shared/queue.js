e5c84d13ab168043965e0f358b3a8a5085a76307
Hack Reactor Students <alexander.gugel@gmail.com>
2014-06-04 16:03:25 -0700

    Passed functional shared pattern queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,44 @@
 var makeQueue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = {};
+
+  newQueue._storage = {};
+  newQueue._size = 0;
+
+  extend(newQueue, queueMethods);
+
+  return newQueue;
 };
 
-var queueMethods = {};
+var extend = function (to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
+};
 
+var queueMethods = {
+  size: function () {
+    return this._size;
+  },
 
+  enqueue: function (val) {
+    this._storage[this._size] = val;
+    this._size += 1;
+  },
+
+  dequeue: function () {
+    var result = this._storage['0'];
+
+    for (var i = 0, l = this._size; i < l; i += 1) {
+      this._storage[i] = this._storage[i + 1];
+    }
+
+    if (this._size > 0) {
+      this._size -= 1;
+    }
+
+    return result;
+  }
+};
 
