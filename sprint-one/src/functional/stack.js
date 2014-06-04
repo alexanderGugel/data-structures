65a7bd5552f96c14ead9587513ec1d221a543a76
Hack Reactor Students <alexander.gugel@gmail.com>
2014-06-04 14:48:00 -0700

    Pass functional pattern stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,25 @@ var makeStack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
   someInstance.push = function(value){
+    storage[size] = value;
+    size++;
   };
 
   someInstance.pop = function(){
+    if (size > 0) {
+      size = size - 1;
+      var result = storage[size];
+      delete storage[size];
+      return result;
+    }
   };
 
   someInstance.size = function(){
+    return size;
   };
 
   return someInstance;
