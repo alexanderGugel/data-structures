00d413e4a88bb7128c8028c95691fae6481ce16c
Hack Reactor Students <alexander.gugel@gmail.com>
2014-06-05 10:30:04 -0700

    Passed pseudoclassical stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,24 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.length = 0;
+  this.storage = {};
 };
 
+Stack.prototype.size = function () {
+  return this.length;
+};
 
+Stack.prototype.push = function (val) {
+  this.storage[this.length] = val;
+  this.length += 1;
+};
+
+Stack.prototype.pop = function () {
+  if (this.length > 0) {
+    var result = this.storage[this.length - 1];
+    delete this.storage[this.length];
+    this.length -= 1;
+    return result;
+  }
+};
