92cb2108babe3547dbef7a3486bd559c181d6732
Hack Reactor Students <alexander.gugel@gmail.com>
2014-06-05 20:50:00 -0700

    Implement linked list (Sprint 2)

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,17 +3,50 @@ var makeLinkedList = function(){
   list.head = null;
   list.tail = null;
 
-  list.addToTail = function(value){
+  list.addToTail = function(val){
+    var linkedListNode = makeNode(val);
+
+    if (list.head === null) {
+      list.head = linkedListNode;
+      list.tail = linkedListNode;
+    } else {
+      var previousTail = list.tail;
+      list.tail = linkedListNode;
+      previousTail.next = linkedListNode;
+    }
   };
 
   list.removeHead = function(){
+    if (list.head !== null) {
+      // List not empty
+      var previousHeadVal = list.head.value;
+      if (list.head.next === null) {
+        // Only one element inside the list
+        list.head = null;
+      } else {
+        list.head = list.head.next;
+      }
+      return previousHeadVal;
+    }
   };
 
   list.contains = function(target){
+    var currentNode = list.head;
+
+    while (currentNode !== null) {
+      if (target === currentNode.value) {
+        return true;
+      }
+      currentNode = currentNode.next;
+    }
+    return false;
   };
 
   return list;
 };
+
+
+
 
 var makeNode = function(value){
   var node = {};
@@ -23,6 +56,7 @@ var makeNode = function(value){
 
   return node;
 };
+
 
 /*
  * Complexity: What is the time complexity of the above functions?
