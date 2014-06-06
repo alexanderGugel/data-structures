c72cca58ac28d5fd73d14f155344f41ffa203e19
Hack Reactor Students <alexander.gugel@gmail.com>
2014-06-06 15:22:00 -0700

    Hash Table (Sprint 2): Collision passed

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,34 +5,83 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  var previous = this._storage.get(i);
+  var bucket = this._storage.get(i);
 
-  if (previous) {
-    if (previous[0] === k) {
-      this._storage.set(i, [k, v]);
+  if (bucket) {
+    if (Array.isArray(bucket[0])) {
+      // Multiple key-val-pairs already inside bucket
+      var index = -1;
+      for (var i = 0; i < bucket.length; i += 1) {
+        if (bucket[i][0] === k) {
+          index = i;
+          break;
+        }
+      }
+      if (index === -1) {
+        // k-v-pair does not exist inside bucket
+        bucket.push([k, v]);
+      } else {
+        bucket[index] = [k, v];
+        // this._storage(i, bucket);
+        // k-v-pair does exist inside bucket
+        // overwrite
+      }
     } else {
-      this._limit = this._limit * 2;
-      var newStorage = makeLimitedArray(this._limit);
-
-      this._storage.each(function (keyValPair, i) {
-        var newIndex = getIndexBelowMaxForKey(keyValPair[0], this._limit);
-        newStorage.set(newIndex, keyValPair);
-      });
-
-      this._storage = newStorage;
-      i = getIndexBelowMaxForKey(k, this._limit);
-      this._storage.set(i, [k, v]);
+      // Exactly ONE k-v-pair inside bucket
+      if (bucket[0] === k) {
+        this._storage.set(i, [k, v]);
+      } else {
+        // Different key-val-pair
+        bucket = [bucket];
+        bucket.push([k, v]);
+        this._storage.set(i, bucket);
+      }
     }
+
+    // if (previous[0] === k) {
+    //   this._storage.set(i, [k, v]);
+    // } else {
+    //   // Collision
+    //   // Something already inside the bucket
+    // }
+
   } else {
+    // Nothing inside bucket
     this._storage.set(i, [k, v]);
   }
+};
+
+HashTable.prototype._double = function() {
+  this._limit = this._limit * 2;
+  var newStorage = makeLimitedArray(this._limit);
+
+  this._storage.each(function (keyValPair, i) {
+    var newIndex = getIndexBelowMaxForKey(keyValPair[0], this._limit);
+    newStorage.set(newIndex, keyValPair);
+  });
+  this._storage = newStorage;
+
+  // i = getIndexBelowMaxForKey(k, this._limit);
+  // this._storage.set(i, [k, v]);
+
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
 
-  if (this._storage.get(i)) {
-    return this._storage.get(i)[1];
+  var bucket = this._storage.get(i);
+
+  if (bucket) {
+    if (Array.isArray(bucket[0])) {
+      for (var i = 0; i < bucket.length; i += 1) {
+        if (bucket[i][0] === k) {
+          return bucket[i][1];
+        }
+      }
+      return null;
+    } else {
+      return bucket[1];
+    }
   } else {
     return null;
   }
