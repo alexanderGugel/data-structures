b711aec498f371a8a5220c42de883ce75e9349c8
Hack Reactor Students <alexander.gugel@gmail.com>
2014-06-06 14:26:54 -0700

    Allowed for doubling of array size

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,17 +5,42 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(i, v);
+  var previous = this._storage.get(i);
+
+  if (previous) {
+    if (previous[0] === k) {
+      this._storage.set(i, [k, v]);
+    } else {
+      this._limit = this._limit * 2;
+      var newStorage = makeLimitedArray(this._limit);
+
+      this._storage.each(function (keyValPair, i) {
+        var newIndex = getIndexBelowMaxForKey(keyValPair[0], this._limit);
+        newStorage.set(newIndex, keyValPair);
+      });
+
+      this._storage = newStorage;
+      i = getIndexBelowMaxForKey(k, this._limit);
+      this._storage.set(i, [k, v]);
+    }
+  } else {
+    this._storage.set(i, [k, v]);
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(i);
+
+  if (this._storage.get(i)) {
+    return this._storage.get(i)[1];
+  } else {
+    return null;
+  }
 };
 
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(i, null);
+  this._storage.set(i, undefined);
 };
 
 
